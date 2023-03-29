c88718a85f6443220c066396a3c59b85d1c8c0b5
Alexey Kovalov <kovalov.alexey@gmail.com>
2023-03-29 22:26:25 +0000

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const CURRENT_TIME = "videoplayer-current-time";
+
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME) || 0);
+
+const updateTime = time => localStorage.setItem(CURRENT_TIME, time.seconds);
+
+player.on('timeupdate', throttle(updateTime, 1000));
 
